39a220fc679363b17219b69f171ec203372bb8f2
Rachel J <racheltyjohnson@gmail.com>
2018-12-06 22:09:29 +1100

    :hammer: Add&JS Register for Activities Section

--- js/script.js
+++ js/script.js
@@ -15,25 +15,51 @@ $(document).ready(function() {
     if( $('#design').val() == "js puns" ){
       $('#color option').hide();
       $('#color option').attr('selected',null);
-
       $('#color option:contains("JS Puns shirt only")').show();
       $("#color option[value*='cornflowerblue']").attr('selected','true');
-
     } else if( $('#design').val() == "heart js" ) {
-
       $('#color option').hide();
       $('#color option').attr('selected',null);
-
       $('#color option:contains("JS shirt only")').show();
       $("#color option[value*='tomato']").attr('selected','true');
-
     } else {
-
       $('#color option').show();
-
     }
   })
 
+  //Register for Activities section
+  let totalCost = 0;
+  $('.totalCost').hide();
+  $('.activities input').change(function(){
 
+    //cost logic
+    if ( $(this).prop('checked') ){
+      totalCost += parseInt( $(this).val() );
+    } else {
+      totalCost -= parseInt( $(this).val() );
+    }
+    if (totalCost != 0){
+      $('.totalCost').text(`Total Cost: $ ${totalCost}`).slideDown();
+    } else {
+      $('.totalCost').slideUp();
+    }
+
+    //competing logic
+    checkActivity('js-frameworks','express');
+    checkActivity('express','js-frameworks');
+    checkActivity('js-libs','node');
+    checkActivity('node','js-libs');
+  })
+
+  //competing activities function
+  function checkActivity(checked,disable){
+    if ( $(`input[name="${checked}"]`).prop('checked') ){
+      $(`input[name="${disable}"]`).parent().addClass('disabled');
+      $(`input[name="${disable}"]`).attr('disabled',true);
+    } else {
+      $(`input[name="${disable}"]`).parent().removeClass('disabled');
+      $(`input[name="${disable}"]`).attr('disabled',null);
+    }
+  }
 
 })
