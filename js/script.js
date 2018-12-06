8cf1cfd848f9d8356c595fe88e100b180430da30
Rachel J <racheltyjohnson@gmail.com>
2018-12-06 21:28:53 +1100

    :hammer: JS TShirt Info Section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
 /*JS file*/
 $(document).ready(function() {
   $("#title").change(function(){
-    if( $('#title').val()=="other"){
+    if( $('#title').val() == "other"){
       $('#other-title').slideDown();
     } else {
       $('#other-title').slideUp();
+    }
+  })
+
+  $("#design").change(function(){
+    if( $('#design').val() == "js puns" ){
+      $('#color option').hide();
+      $('#color option').attr('selected',null);
+      
+      $('#color option:contains("JS Puns shirt only")').show();
+      $("#color option[value*='cornflowerblue']").attr('selected','true');
+
+    } else if( $('#design').val() == "heart js" ) {
+
+      $('#color option').hide();
+      $('#color option').attr('selected',null);
+
+      $('#color option:contains("JS shirt only")').show();
+      $("#color option[value*='tomato']").attr('selected','true');
+
+    } else {
+
+      $('#color option').show();
+
     }
   })
 })
