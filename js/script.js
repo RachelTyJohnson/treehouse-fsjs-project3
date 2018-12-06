74f490f218206c247cb488067ac15567d6b5664a
Rachel J <racheltyjohnson@gmail.com>
2018-12-06 21:15:37 +1100

    :hammer: Add Job Role Section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 /*JS file*/
+$(document).ready(function() {
+  $("#title").change(function(){
+    if( $('#title').val()=="other"){
+      $('#other-title').slideDown();
+    } else {
+      $('#other-title').slideUp();
+    }
+  })
+})
